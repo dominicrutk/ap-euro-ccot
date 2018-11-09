b948e1441e7fcf46cd318b266d9da85a95f4f2ca
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-11-09 02:39:16 -0600

    Thirty Years' War

--- events.js
+++ events.js
@@ -40,5 +40,28 @@ const events = {
                 text: `On October 31, 1517, to express his displeasure with the Catholic Church, Luther nailed his <em>Ninety-fve Theses</em> to a church door in Wittenberg. The <em>Ninety-five Theses</em> outlined Luther's argument against the sale of indulgences. This October day is often considered the beginning of the Protestant Reformation, in which new religions such as Lutheranism and Calvinism split from Catholicism. The Protestant Reformation fundamentally changed the religious makeup of Europe, and it created a scapegoat for political wars in the 17<sup>th</sup> century.`
             }
         ]
+    },
+    thirtyYearsWar: {
+        name: `Thirty Years' War`,
+        startYear: 1618,
+        endYear: 1648,
+        latitude: 51.9625,
+        longitude: 7.625556,
+        category: 'change',
+        description: [
+            {
+                type: 'p',
+                text: `The Thirty Years' War broke out in 1618 in Bohemia. The Holy Roman Emperor, Ferdinand, attempted to impose Catholic rule on Bohemian Protestants, but he was met with resistance. Over time, the war escalated, as it progressed through its four phases. What was initially a local, religious war became a continental, political war between the Habsburgs and the anti-Habsburg alliance of Sweden, Denmark, the Netherlands, several Holy Roman Empire territories, and France.`
+            },
+            {
+                type: 'p',
+                text: `The war concluded in 1648 with the Peace of Westphalia. The Peace of Westphalia restored the Peace of Augsburg of 1555, which allowed local rulers to determine the religion of their territories. In addition, Calvinism joined Catholicism and Lutheranism as a tolerated religion. The Peace of Westphalia provided a period of peace, albeit brief. It marked the end of the Reformation era.`
+            },
+            {
+                type: 'img',
+                image: './img/holyRomanEmpire.png',
+                caption: 'The Peace of Westphalia gave each local prince the power to choose the religion of their domain, thus greatly strengthening the nobility at the expense of the Holy Roman Emperor. <a href="http://www.wikiwand.com/en/Holy_Roman_Empire">Image source</a>'
+            }
+        ]
     }
 };
