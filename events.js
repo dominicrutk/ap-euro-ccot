38abac116b67ddd2c053600bb52990677119e414
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-11-09 02:14:06 -0600

    Columbus and Luther events

--- events.js
+++ events.js
@@ -1,51 +1,43 @@
 const events = {
-    event1: {
-        name: "Event 1",
-        startYear: 1599,
-        endYear: 1600,
-        latitude: 0.0,
-        longitude: 0.0,
-        category: "continuity",
+    columbusLandsInAmerica: {
+        name: 'Columbus Discovers America',
+        startYear: 1492,
+        endYear: 1492,
+        latitude: 22.707657,
+        longitude: -73.895284,
+        category: 'change',
         description: [
             {
-                type: "p",
-                text: "This is a paragraph."
+                type: 'p',
+                text: `On October 12, 1492, Christopher Columbus and his crew aboard the <em>Pinta</em> spotted land. They had discovered San Salvador (the Bahamas), which is part of the New World. This discovery allowed for Spain, and the other European countries shortly thereafter, to exploit indigenous peoples for labor. In addition, American goods could be exported to Europe in the Columbian Exchange. Columbus's discovery of the New World marked a turning point in European history as the wealth of countries quickly swelled from the riches to be found in the Americas.`
             },
             {
-                type: "p",
-                text: "This is another paragraph."
-            },
-            {
-                type: "p",
-                text: "This is a paragraph."
-            },
-            {
-                type: "img",
-                image: "https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTU3ODc5MDg2NDI5Nzc1MTc3/antonio-de-pereda-thirty-years-war.jpg",
-                caption: "This is a caption for the image."
-            },
-            {
-                type: "p",
-                text: "This is the last paragraph."
+                type: 'img',
+                image: './img/columbusFleet.jpg',
+                caption: `Christopher Columbus's fleet of three ships: <em>La Niña</em>, <em>La Pinta</em>, and <em>Santa María</em>. <a href="https://owlcation.com/humanities/Whatever-Happened-to-the-Nia-Pinta-and-Santa-Maria">Image source</a>`
             }
         ]
     },
-    event2: {
-        name: "Event 2",
-        startYear: 1618,
-        endYear: 1648,
-        latitude: 10.0,
-        longitude: 10.0,
-        category: "change",
+    ninetyFiveTheses: {
+        name: `Martin Luther's Ninety-five Theses`,
+        startYear: 1517,
+        endYear: 1517,
+        latitude: 51.866389,
+        longitude: 12.637778,
+        category: 'change',
         description: [
             {
-                type: "p",
-                text: "This is a paragraph."
+                type: 'p',
+                text: 'Indulgences were a practice in which the Catholic Church sold the good deeds of saints to ordinary people looking to avoid purgatory. They were often used by families with unbaptized babies that would otherwise end up in limbo. They cleansed the people that they were used on of (original) sin. Martin Luther, a German theologian, detested the sale of indulgences because it was an example of corruption in the Catholic Church.'
             },
             {
-                type: "img",
-                image: "https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTU3ODc5MDg2NDI5Nzc1MTc3/antonio-de-pereda-thirty-years-war.jpg",
-                caption: "This is a caption for the image."
+                type: 'img',
+                image: './img/allSaintsChurchWittenberg.jpg',
+                caption: 'The church door to which Luther nailed his <em>Ninety-five Theses</em> in the modern day. <a href="https://fotoeins.com/2017/01/16/lutherstadt-wittenberg-schlosskirche-castle-church/">Image source</a>'
+            },
+            {
+                type: 'p',
+                text: `On October 31, 1517, to express his displeasure with the Catholic Church, Luther nailed his <em>Ninety-fve Theses</em> to a church door in Wittenberg. The <em>Ninety-five Theses</em> outlined Luther's argument against the sale of indulgences. This October day is often considered the beginning of the Protestant Reformation, in which new religions such as Lutheranism and Calvinism split from Catholicism. The Protestant Reformation fundamentally changed the religious makeup of Europe, and it created a scapegoat for political wars in the 17<sup>th</sup> century.`
             }
         ]
     }
